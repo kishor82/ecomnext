08acac747a74a2a1008fdda2727aac5bb86d5d81
kishor82 <kishorrathva8298@gmail.com>
2023-12-21 19:29:08 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Your marketplace for high-quality {""}
+          <span className="text-blue-600">digital assets</span>.
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          voluptate quos totam commodi omnis? Odio sequi, sunt sed nobis
+          numquam, hic odit eveniet exercitationem doloribus laudantium
+          dignissimos perferendis cumque mollitia!
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href="/products" className={buttonVariants()}>
+            Browse Trending
+          </Link>
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  );
+}
